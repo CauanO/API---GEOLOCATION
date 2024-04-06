840ce565e94594d1e00dfe0c5f2a6d01896c7281
Cauan Oliveira <134656319+CauanO@users.noreply.github.com>
2024-04-06 10:15:36 -0300

    alteração-iframe+api-status:incompleto

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-if ('geolocation' in navigator) {
+if ('geolocation' in navigator ) {
     navigator.geolocation.getCurrentPosition(function(position){
         var logitude = position.coords.longitude
         var latitude = position.coords.latitude
-        document.getElementById('logitude').innerHTML = `Sua logitude é ${logitude}`
-        document.getElementById('latitude').innerHTML = `Sua latitude é ${latitude}`
+        var mapUrl = "https://www.google.com/maps/embed/v1/view?key=YOUR_API_KEY&center=" + latitude + "," + logitude + "&zoom=15";
+        document.getElementById('mapFrame').src = mapUrl
+        console.log(position)
 
     }, function(error){
         document.getElementById('latitude').innerHTML = `Parabens, passou pelo teste...`
